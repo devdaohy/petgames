3d06646bb0e43331c6c4809218c83c84953fee1a
Nguyen Ba Thai <nguyenbathai.nbt@gmail.com>
2021-08-23 13:33:49 +0700

    a thai sua

--- js/fanpage.js
+++
@@ -1,6 +0,0 @@
-$(window)
-    .resize(function () {
-        var height = $(window).height();
-        height = height -100;
-        $(".store").attr("style","height:"+height +"px");
-    }).resize();
